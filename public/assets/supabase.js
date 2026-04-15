4278de64b682b1c7a7a23b93856fcc6798f0fc21
ajulayfi <36624655+ajulayfi@users.noreply.github.com>
2026-04-15 15:51:56 +0300

    Update supabase.js

--- public/assets/supabase.js
+++ public/assets/supabase.js
@@ -1,7 +1,7 @@
 
 // 🔐 بيانات Supabase (من Project Settings → API)
-export const SUPABASE_URL = "https://gspubqodpuxdlvwlmhvo.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdzcHVicW9kcHV4ZGx2d2xtaHZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4MzgxNDYsImV4cCI6MjA4NzQxNDE0Nn0.iuc_7G14hHurPAZOQ9eR2O8iBmTOqlOM1z23oBZqyI4";
+const SUPABASE_URL = "https://gspubqodpuxdlvwlmhvo.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdzcHVicW9kcHV4ZGx2d2xtaHZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4MzgxNDYsImV4cCI6MjA4NzQxNDE0Nn0.iuc_7G14hHurPAZOQ9eR2O8iBmTOqlOM1z23oBZqyI4";
 
 // ===============================
 // Dawriya Rest - Configuration
