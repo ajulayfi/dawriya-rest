ece70bdefd9f328b3925bf8141b2475c17d1d8d0
ajulayfi <36624655+ajulayfi@users.noreply.github.com>
2026-04-15 13:38:08 +0300

    Update supabase.js

--- public/assets/supabase.js
+++ public/assets/supabase.js
@@ -1,4 +1,10 @@
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+
+// 🔐 بيانات Supabase (من Project Settings → API)
+export const SUPABASE_URL = "https://gspubqodpuxdlvwlmhvo.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdzcHVicW9kcHV4ZGx2d2xtaHZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4MzgxNDYsImV4cCI6MjA4NzQxNDE0Nn0.iuc_7G14hHurPAZOQ9eR2O8iBmTOqlOM1z23oBZqyI4";
+
+// ===============================
+// Dawriya Rest - Configuration
+// ===============================
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
